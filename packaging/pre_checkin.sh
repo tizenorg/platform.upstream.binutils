3cd91fe987f8b83aa282b9b2b63dbe31427bff50
Anas Nashif <anas.nashif@intel.com>
2012-10-30 12:48:49 -0700

    add packaging

---
+++ packaging/pre_checkin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# the script takes binutils* and creates the cross-* packages
+# sh4 is stuck in the testsuite
+for arch in arm i386 x86_64; do 
+
+   echo -n "Building package for $arch --> cross-$arch-binutils ..."
+
+   #ln -f binutils.changes cross-$arch-binutils.changes
+   targetarch=`echo $arch | sed -e "s/parisc/hppa/;s/i.86/i586/;s/ppc/powerpc/"`
+   exclarch=`echo $arch | sed -e 's/parisc/hppa/;s/i.86/%ix86/;s/arm/%arm/'`
+   sed -e "s/^Name:.*binutils\$/Name:         cross-$arch-binutils\nExcludeArch: $exclarch\n%define cross 1\n%define TARGET $targetarch/;" \
+       < binutils.spec > cross-$arch-binutils.spec
+
+   echo " done."
+done
+
+osc service localrun format_spec_file
+
