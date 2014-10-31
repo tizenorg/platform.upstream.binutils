86a98358063344e0f431e2a28da5123be0ef9272
Vyacheslav Barinov <v.barinov@samsung.com>
2014-10-31 16:13:01 +0300

    Switching ExcludeArch to ExclusiveArch
    Helps to avoid cross-arm -> aarch64 and cross-aarch64 -> arm builds.
    
    Change-Id: Iea31ec9aec7d4ddceeb0401bf820135f342fad30
    Signed-off-by: Vyacheslav Barinov <v.barinov@samsung.com>

--- packaging/pre_checkin.sh
+++ packaging/pre_checkin.sh
@@ -8,8 +8,7 @@ for arch in arm aarch64 i386 x86_64; do
 
    #ln -f binutils.changes cross-$arch-binutils.changes
    targetarch=`echo $arch | sed -e "s/parisc/hppa/;s/i.86/i586/;s/ppc/powerpc/"`
-   exclarch=`echo $arch | sed -e 's/parisc/hppa/;s/i.86/%ix86/;s/arm/%arm/'`
-   sed -e "s/^Name:.*binutils\$/Name:         cross-$arch-binutils\nExcludeArch: $exclarch\n%define cross 1\n%define TARGET $targetarch/;" \
+   sed -e "s/^Name:.*binutils\$/Name:\t\tcross-$arch-binutils\nExclusiveArch: %{ix86} x86_64\n%define cross 1\n%define TARGET $targetarch/;" \
        binutils.spec | sed '/manifest/d' |sed '/1001/d' > cross-$arch-binutils.spec
    echo " done."
 done
