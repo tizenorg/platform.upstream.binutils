dfd879c4e578d0533dbf83bb0fb2df6ccfa57862
Yury Usishchev <y.usishchev@samsung.com>
2015-12-11 19:51:54 +0300

    Revert "Ignore missing plugins"
    This reverts commit b7bbada7510f59181a965bd951bf97d974ddb9cc.

--- bfd/plugin.c
+++ bfd/plugin.c
@@ -294,6 +294,7 @@ load_plugin (bfd *abfd)
     {
       char *full_name;
       struct stat s;
+
       full_name = concat (p, "/", ent->d_name, NULL);
       if (stat(full_name, &s) == 0 && S_ISREG (s.st_mode))
 	found = try_load_plugin (full_name, abfd);
